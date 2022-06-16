c96916b90c724c7e9d73f1a39dcd87c8bab2b135
Miles Garnsey <11435896+Miles-Garnsey@users.noreply.github.com>
2022-06-16 16:57:28 +1000

    Quality of life engancements (#8)
    * Remove extraneous Dockerfile.
    * Add some output indicating success.
    * Allow setting separate contact point for Pulsar's Cassandra connection vs this app's.
    * Fix error in pulsar admin URL default parm.
    * Allow setting Cassandra DC name.
    * Fix error in default cassandra contact points.
    * More Pulsar contact point errors fixed in default settings.
    * Drop RF to 1 since this is a test.
    * Use Logback for logging.
    * version bump to SNAPSHOT and add release instructions to README.

--- pulsar-cdc-testutil/app/src/main/scala/io/k8ssandra/pulsarcdctestutil/RunTests.scala
+++ pulsar-cdc-testutil/app/src/main/scala/io/k8ssandra/pulsarcdctestutil/RunTests.scala
@@ -1,8 +1,8 @@
 package io.k8ssandra.pulsarcdctestutil
 
-import com.sksamuel.pulsar4s.PulsarClient
 import picocli.CommandLine._
-
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.util.concurrent.Callable
 
 @Command(
@@ -18,22 +18,32 @@ class RunTests extends Callable[Int] {
     names = Array("--pulsar-url"),
     description = Array("Pulsar URL.")
   )
-  private var pulsarURL = "pulsar://pulsar-proxy.pulsar.svc.cluster.local:5600"
+  private var pulsarURL = "pulsar://pulsar-proxy.pulsar.svc.cluster.local:6650"
+
   @Option(
     names = Array("--schema-registry-url"),
     description = Array("Schema Registry URL.")
   )
-  private var schemaRegistryURL = "pulsar://pulsar-proxy.pulsar.svc.cluster.local:5600"
+  private var schemaRegistryURL = "pulsar://pulsar-proxy.pulsar.svc.cluster.local:8080"
+
   @Option(
     names = Array("--pulsar-admin-url"),
     description = Array("Pulsar admin URL.")
   )
-  private var pulsarAdminURL = "pulsar://pulsar-proxy.pulsar.svc.cluster.local:8080"
+  private var pulsarAdminURL = "http://pulsar-proxy.pulsar.svc.cluster.local:8080"
+
   @Option(
     names = Array("--pulsar-topic"),
     description = Array("Pulsar topic.")
   )
   private var pulsarTopic ="persistent://public/default/data-db1.table1"
+
+  @Option(
+    names = Array("--pulsar-cass-contact-point"),
+    description = Array("Pulsar's Cassandra contact points (as opposed to this application's.")
+  ) // This is needed in case the contact points available to Pulsar aren't the same as those available to this application.
+  private var PulsarCassContactPoints =cassandraContactPoints
+
   @Option(
     names = Array("--pulsar-auth-class"),
     description = Array("Pulsar Auth plugin class.")
@@ -49,18 +59,31 @@ class RunTests extends Callable[Int] {
     names = Array("--cass-contact-points"),
     description = Array("Cassandra contact point.")
   )
-  private var cassandraContactPoints = "cluster2-dc1-all-pods-service.default.svc.cluster.local"
+  private var cassandraContactPoints = "test-cluster-dc1-all-pods-servicecass-operator.svc.cluster.local:9042"
+
+  @Option(
+    names = Array("--cass-dc"),
+    description = Array("Cassandra datacenter name.")
+  )
+  private var cassDCName = "dc1"
+
   @Option(
     names = Array("--cass-auth-class"),
     description = Array("Cassandra auth plugin class.")
   )
   private var cassAuthClass = ""
+
   @Option(
     names = Array("--cass-auth-parms"),
     description = Array("comma delimited Cassandra auth params.")
   )
   private var cassAuthParms = ""
+
+  val logger: Logger = LoggerFactory.getLogger(classOf[RunTests])
+
   override def call(): Int = {
+    logger.info("Starting application.")
+
     val cassClient = CassandraOrchestrator(
       cassandraContactPoints,
       cassAuthClass,
@@ -74,31 +97,31 @@ class RunTests extends Callable[Int] {
       pulsarAuthParms)
 
     // Create tables in Cassandra
-    var res : Either[Throwable, _] = cassClient.migrate("dc1")
-    if (res.isLeft) { println("Failed to create Cassandra schema"); return ExitCode.SOFTWARE }
+    var res : Either[Throwable, _] = cassClient.migrate(cassDCName)
+    if (res.isLeft) { logger.error("Failed to create Cassandra schema"); return ExitCode.SOFTWARE }
 
     // Configure Pulsar connector.
     res = pulsarClient.connectorConfigure(
-      cassDC="dc1",
-      cassContactPoint="cluster2-dc1-all-pods-service.default.svc.cluster.local",
+      cassDC=cassDCName,
+      cassContactPoint=PulsarCassContactPoints,
       keyspace="db1",
       table="table1"
     )
     if (res.isLeft) {
-      println(s"Failed to configure Pulsar connector: ${res.left.get.getMessage}")
+      logger.error(s"Failed to configure Pulsar connector: ${res.left.get.getMessage}")
       return ExitCode.SOFTWARE
     }
 
     // Add data to Cassandra
-    res = cassClient.addData("dc1")
+    res = cassClient.addData(cassDCName)
     if (res.isLeft) {
-      println(s"Failed to add data to Cassandra: ${res.left.get.getMessage}")
+      logger.error(s"Failed to add data to Cassandra: ${res.left.get.getMessage}")
       return ExitCode.SOFTWARE
     }
 
     val maybeData = pulsarClient.fetchData()
     if (maybeData.isLeft) {
-      println(s"Data fetch on topic failed: ${maybeData.left.get.getMessage}")
+      logger.error(s"Data fetch on topic failed: ${maybeData.left.get.getMessage}")
       return ExitCode.SOFTWARE
     }
 
@@ -107,10 +130,10 @@ class RunTests extends Callable[Int] {
     // Check data from Pulsar
     pulsarClient.checkData(data)
     if (res.isLeft) {
-      println(s"Data validation on topic failed: ${res.left.get.getMessage}")
+      logger.error(s"Data validation on topic failed: ${res.left.get.getMessage}")
       return ExitCode.SOFTWARE
     }
-
+    logger.info("SUCCESS")
     ExitCode.OK
   }
 }
